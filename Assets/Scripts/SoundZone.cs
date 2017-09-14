db1de705ea4cf8489fafb8f5aae5d9d70e9a6437
Zachary De Maria <s171558@ad.aie.edu>
2017-09-14 14:26:13 +0930

    Sound manager

---
+++ Assets/Scripts/SoundZone.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class SoundZone : MonoBehaviour {
+
+    public AudioSource soundPrefab;
+    Dictionary<GameObject, AudioSource> sounds;
+    SphereCollider c;
+
+	// Use this for initialization
+	void Start () {
+        sounds = new Dictionary<GameObject, AudioSource>();
+        c = GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (var sound in sounds)
+        {
+            sound.Value.gameObject.transform.position = transform.position - sound.Key.transform.position;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        sounds.Add(other.gameObject, Instantiate(soundPrefab));
+        sounds[other.gameObject].maxDistance = c.radius;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(sounds[other.gameObject].gameObject);
+        sounds.Remove(other.gameObject);
+    }
+}
